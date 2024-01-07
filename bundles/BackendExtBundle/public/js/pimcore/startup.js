edce09d923d24ab6d227c5a3b5c1d375445432ff
siwachs <ssiwach.8888@gmail.com>
2024-01-07 20:50:09 +0530

    Ext JS model and iframes

--- bundles/BackendExtBundle/public/js/pimcore/startup.js
+++ bundles/BackendExtBundle/public/js/pimcore/startup.js
@@ -26,9 +26,11 @@ pimcore.plugin.BackendExtBundle = Class.create({
     },
 
     loadTailwindCSS: function () {
-        const script = document.createElement("script");
-        script.src = "https://cdn.tailwindcss.com";
-        document.body.append(script);
+        const link = document.createElement("link");
+        link.href =
+            "https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css";
+        link.rel = "stylesheet";
+        document.head.appendChild(link);
     },
 
     preMenuBuild: function (e) {
@@ -43,16 +45,60 @@ pimcore.plugin.BackendExtBundle = Class.create({
             shadow: false,
             handler: this.addUserForAProductDataEntryHandler.bind(this),
             noSubmenus: true,
-            cls: "white-icon",
         };
     },
 
     addUserForAProductDataEntryHandler: function (e) {
         const dialog = document.createElement("dialog");
+        const card = document.createElement("div");
+        const title = document.createElement("h3");
         const iframe = document.createElement("iframe");
-        iframe.setAttribute("src", "https://www.youtube.com/embed/R0OD5F-WO9A");
+        const closeIcon = document.createElement("i");
 
-        dialog.appendChild(iframe);
+        card.classList.add(
+            "w-full",
+            "bg-white",
+            "shadow-md",
+            "rounded",
+            "mx-auto",
+            "p-4",
+            "relative"
+        );
+
+        title.classList.add("font-bold", "text-xl");
+        title.innerHTML = "Assign user to a product";
+
+        closeIcon.classList.add(
+            "fas",
+            "fa-times",
+            "text-black",
+            "absolute",
+            "top-4",
+            "right-4",
+            "cursor-pointer",
+            "text-xl"
+        );
+        closeIcon.addEventListener("click", function () {
+            dialog.remove();
+            dialog.innerHTML = "";
+            closeIcon.removeEventListener("click", null);
+        });
+
+        iframe.classList.add(
+            "w-full",
+            "h-full",
+            "border",
+            "border-gray-200",
+            "mt-4"
+        );
+        iframe.name = "assign_user";
+        iframe.title = "Assign user to a product";
+        iframe.setAttribute("src", "http://cms.com/backend_ext");
+
+        card.appendChild(title);
+        card.appendChild(closeIcon);
+        card.appendChild(iframe);
+        dialog.appendChild(card);
         document.body.appendChild(dialog);
         dialog.showModal();
     },
